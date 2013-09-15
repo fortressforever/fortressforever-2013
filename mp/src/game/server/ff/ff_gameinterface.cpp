e287b1516a527c4f31815c845e01c5f2a8d53587
Dexter Haslem <dexter.haslem@gmail.com>
2013-09-14 22:44:17 -0600

    hello FF world
    - had to move HL2DM VPC stuff INTO ff server vpc so we can start (re)moving
    nonspecific code as it is implemented

---
+++ mp/src/game/server/ff/ff_gameinterface.cpp
@@ -0,0 +1,34 @@
+//========= Copyright Valve Corporation, All rights reserved. ============//
+//
+// Purpose: 
+//
+//=============================================================================//
+
+#include "cbase.h"
+#include "gameinterface.h"
+#include "mapentities.h"
+#include "hl2mp_gameinterface.h"
+
+// memdbgon must be the last include file in a .cpp file!!!
+#include "tier0/memdbgon.h"
+
+// -------------------------------------------------------------------------------------------- //
+// Mod-specific CServerGameClients implementation.
+// -------------------------------------------------------------------------------------------- //
+
+void CServerGameClients::GetPlayerLimits( int& minplayers, int& maxplayers, int &defaultMaxPlayers ) const
+{
+	minplayers = 2; 
+	maxplayers = MAX_PLAYERS; // TODO: currently 33
+	defaultMaxPlayers = 16;
+}
+
+// -------------------------------------------------------------------------------------------- //
+// Mod-specific CServerGameDLL implementation.
+// -------------------------------------------------------------------------------------------- //
+
+void CServerGameDLL::LevelInit_ParseAllEntities( const char *pMapEntities )
+{
+	// TODO: FF map entity parse for stuff to keep around after reset
+}
+
---
+++ mp/src/game/server/ff/ff_scriptman.cpp
@@ -0,0 +1,34 @@
+
+/////////////////////////////////////////////////////////////////////////////
+// includes
+#include "cbase.h"
+//#include "ff_scriptman.h"
+//#include "ff_entity_system.h"
+//#include "ff_luacontext.h"
+//#include "ff_lualib.h"
+//#include "ff_utils.h"
+//#include "ff_item_flag.h"
+#include "triggers.h"
+
+// engine
+#include "filesystem.h"
+
+// lua
+//extern "C"
+//{
+//	#include "lua.h"
+//	#include "lualib.h"
+//	#include "lauxlib.h"
+//}
+//
+//#undef MINMAX_H
+//#undef min
+//#undef max
+//
+//// luabind
+//#include "luabind/luabind.hpp"
+//#include "luabind/object.hpp"
+//#include "luabind/iterator_policy.hpp"
+
+// memdbgon must be the last include file in a .cpp file!!!
+#include "tier0/memdbgon.h"
