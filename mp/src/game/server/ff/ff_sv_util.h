67c8b23b93c166f1ac4e60d34e368e37ee18226a
hlstriker <hlstriker@gmail.com>
2013-11-04 03:18:30 +0000

    Added wrapper functions for HUD messages.

---
+++ mp/src/game/server/ff/ff_sv_util.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#ifndef FF_SV_UTIL_H
+#define FF_SV_UTIL_H
+
+// HUD Messages
+typedef enum 
+{
+	HUD_EFFECT_FADE_IN_OUT = 0,
+	HUD_EFFECT_FLICKER,
+	HUD_EFFECT_WRITE_OUT
+} HUD_MSG_EFFECT;
+
+void	FF_UTIL_HudMessageAll( float x, float y, HUD_MSG_EFFECT effect, color32 color1, color32 color2, float fadeInTime, float fadeOutTime, float holdTime, float fxTime, int channel, const char *pszFormat, ... );
+void	FF_UTIL_HudMessage( CBasePlayer *pToPlayer, float x, float y, HUD_MSG_EFFECT effect, color32 color1, color32 color2, float fadeInTime, float fadeOutTime, float holdTime, float fxTime, int channel, const char *pszFormat, ... );
+
+
+#endif //FF_SV_UTIL_H
