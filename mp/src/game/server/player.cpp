6c08c02252041fc28b4102b727b35ca466b89b5c
squeek <squeek502@hotmail.com>
2013-12-03 04:24:17 +0000

    Lag compensate for friendly players even when friendly-fire is off. Mostly a precaution. Note that CFF_SV_Player implements its own WantsLagCompensationOnEntity.
    See: https://github.com/ValveSoftware/source-sdk-2013/issues/201#issuecomment-29565874

--- mp/src/game/server/player.cpp
+++ mp/src/game/server/player.cpp
@@ -735,8 +735,12 @@ int CBasePlayer::ShouldTransmit( const CCheckTransmitInfo *pInfo )
 bool CBasePlayer::WantsLagCompensationOnEntity( const CBasePlayer *pPlayer, const CUserCmd *pCmd, const CBitVec<MAX_EDICTS> *pEntityTransmitBits ) const
 {
 	// Team members shouldn't be adjusted unless friendly fire is on.
+	// FF --> Disagree
+	/*
 	if ( !friendlyfire.GetInt() && pPlayer->GetTeamNumber() == GetTeamNumber() )
 		return false;
+	*/
+	// FF <--
 
 	// If this entity hasn't been transmitted to us and acked, then don't bother lag compensating it.
 	if ( pEntityTransmitBits && !pEntityTransmitBits->Get( pPlayer->entindex() ) )
